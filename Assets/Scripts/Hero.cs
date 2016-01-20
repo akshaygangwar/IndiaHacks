c26d60d40eecdb088eab5238d359c1b14de44826
Akshay Gangwar <akshaygangwar2009@gmail.com>
2016-01-20 20:42:01 +0530

    Prefabs created, hint boxes, doors added.

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -8,9 +8,11 @@ public class Hero : MonoBehaviour {
 
 	private Text textBox;
 	private bool isUpEnabled = true;
+	private int randomizer;
 
 	// Use this for initialization
 	void Start () {
+		textBox = FindObjectOfType<Text>();
 	}
 	
 	// Update is called once per frame
@@ -44,16 +46,50 @@ public class Hero : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D collider) {
 		GameObject obj = collider.gameObject;
-		if (obj.tag == "Door_One") {			//If door one is triggered.
+
+		if (obj.tag == "Door_One") {			//If door one is triggered, head to levels that lead to freedom
+
 			Debug.Log ("Door 1");
-		} else if (obj.tag == "Door_Two") {		//If door two is triggered.
+
+			//Leads towards freedom
+			Application.LoadLevel("Level_2");
+
+		} else if (obj.tag == "Door_Two") {		//If door two is triggered, random level loads.
+
 			Debug.Log ("Door Two");
-		} else if (obj.tag == "Door_Three") {	//If door three is triggered.
+			//Leads towards any of the two
+
+			randomizer = Random.Range(0,3);
+
+			if (randomizer == 0) {
+				//Load same level
+				Application.LoadLevel("Level 1");
+			}
+
+			else if (randomizer == 1) {
+				//Load Level that leads to freedom
+				Debug.Log ("Load Level 2");
+			}
+
+			else {
+				//Load level that goes further deep
+				Debug.Log ("Load Level 3");
+			}
+
+			Debug.Log (randomizer.ToString());
+
+		} else if (obj.tag == "Door_Three") {	//If door three is triggered, head deeper inside.
+
 			Debug.Log ("Door Three");
+
+			//Leads further deep
+
 		} else if (obj.tag == "LevelGeo") {		//If level geometry is triggered, such as walls.
+
 			isUpEnabled = false;
+
 		} else if (obj.tag == "Box") {			//If hint boxes/scrolls are triggered.
-			textBox = FindObjectOfType<Text>();
+
 			textBox.text = "Three doors you will see, one of them will help you get free. One will lead you further, deep and one " +
 				"will always change where it leads.";
 		}
